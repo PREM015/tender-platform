835d8bcd7a647f8679d45b25f9e6f6eca8406cf8
PREM015 <152625984+PREM015@users.noreply.github.com>
2025-07-04 10:40:41 +0530

    feat: remove unused files and restructure API and Supabase client setup

--- frontend/src/app/lib/supabaseClient.js
+++ frontend/src/app/lib/supabaseClient.js
@@ -1,7 +1,10 @@
-// src/lib/supabaseClient.js
+
+
 import { createClient } from "@supabase/supabase-js";
 
+// These should come from `.env.local`
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+// Create and export the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
